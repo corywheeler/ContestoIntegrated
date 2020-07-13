e97c38b56b8bbe14200c091d17d211c4c1171b9d
Cory Wheeler <cory@howardcm.com>
2020-07-13 13:39:24 -0400

    Generate and modify EF Generation from DB via Powershell Command
    Scaffold-DbContext "Server=(localdb)\MSSQLLocalDB;Database=ContosoUniversity1;Trusted_Connection=True;" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models
    
    Things were Pluralized and additional mapping added into OnCreateDatabase

---
+++ ContestoIntegrated/Data/SchoolContext.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using ContestoIntegrated.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ContestoIntegrated.Data
+{
+    public partial class SchoolContext : DbContext
+    {
+        public SchoolContext()
+        {
+        }
+
+        public SchoolContext(DbContextOptions<SchoolContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Course> Courses { get; set; }
+        public virtual DbSet<Enrollment> Enrollments { get; set; }
+        public virtual DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=ContosoUniversity1;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.6-servicing-10079");
+
+			modelBuilder.Entity<Course>().ToTable("Course");
+			modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+			modelBuilder.Entity<Student>().ToTable("Student");
+
+			modelBuilder.Entity<Course>(entity =>
+            {
+                entity.Property(e => e.CourseID)
+                    .HasColumnName("CourseID")
+                    .ValueGeneratedNever();
+            });
+
+            modelBuilder.Entity<Enrollment>(entity =>
+            {
+                entity.HasIndex(e => e.CourseID);
+
+                entity.HasIndex(e => e.StudentID);
+
+                entity.Property(e => e.EnrollmentID).HasColumnName("EnrollmentID");
+
+                entity.Property(e => e.CourseID).HasColumnName("CourseID");
+
+                entity.Property(e => e.StudentID).HasColumnName("StudentID");
+
+                entity.HasOne(d => d.Course)
+                    .WithMany(p => p.Enrollments)
+                    .HasForeignKey(d => d.CourseID);
+
+                entity.HasOne(d => d.Student)
+                    .WithMany(p => p.Enrollments)
+                    .HasForeignKey(d => d.StudentID);
+            });
+
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.Property(e => e.ID).HasColumnName("ID");
+            });
+        }
+    }
+}
---
+++ ContestoIntegrated/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ContestoIntegrated.Models
+{
+    public class Student
+    {
+        public Student()
+        {
+            Enrollments = new HashSet<Enrollment>();
+        }
+
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
